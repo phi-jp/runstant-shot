e5befda7e3c4a724607cb9b4869b43c458ab609b
phi <phi1618jp@gmail.com>
2016-02-22 22:13:54 +0900

    fix t

--- t/slimer.js
+++ t/slimer.js
@@ -6,6 +6,6 @@ var url = args[1] || 'http://phiary.me';
 
 page.open(url, function (status) {
   page.viewportSize = { width:1024, height:768 };
-  page.render('static/screenshot.png');
+  page.render('static/images/screenshot.png');
   slimer.exit();
 });
