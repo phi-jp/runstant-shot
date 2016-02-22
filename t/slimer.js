fe137fadae668a51af9b029b2583fb2384f5ea5e
phi <phi1618jp@gmail.com>
2016-02-22 17:11:06 +0900

    fix

--- t/slimer.js
+++ t/slimer.js
@@ -2,5 +2,6 @@ var page = require('webpage').create();
 
 page.open("http://phiary.me", function (status) {
   page.viewportSize = { width:1024, height:768 };
-  page.render('screenshot.png')
+  page.render('screenshot.png');
+  slimer.exit();
 });
