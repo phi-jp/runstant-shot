1eda45843b18ba20e649ab24c3ff238742b810b3
phi <phi1618jp@gmail.com>
2016-02-23 01:24:07 +0900

    fix buildpacks

--- t/slimer.js
+++ t/slimer.js
@@ -5,7 +5,8 @@ var args = system.args;
 var url = args[1] || 'http://phiary.me';
 
 page.open(url, function (status) {
-  page.viewportSize = { width:1024, height:768 };
+  slimer.wait(4000)
+  // page.viewportSize = { width:1024, height:768 };
   page.render('static/images/screenshot.png');
   slimer.exit();
 });
