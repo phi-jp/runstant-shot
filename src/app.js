3f8a6d38a7d18fe2e4acdaf4196d1366d8332739
phi <phi1618jp@gmail.com>
2016-02-24 13:57:28 +0900

    use s3

--- src/app.js
+++ src/app.js
@@ -25,30 +25,18 @@ var phantomPath = (function() {
 
 // aws
 var AWS = require('aws-sdk');
-AWS.config.update({
-  // region: 'Tokyo',
-  accessKeyId: 'aaa',
-  secretAccessKey: 'bbb',
-});
-// var s3 = new AWS.S3();
-
-// s3.putObject({
-//   Bucket: 'runstant-shot',
-//   Key: 'foo.txt',
-//   Body: 'Hello, world!',
-// }, function(err, url) {
-//   console.log(err);
-//   console.log(url);
-//   console.log('Successfully uploaded package.');
-// });
-
-// s3.getObject({
-//   Bucket: 'runstant-shot',
-//   Key: 'dummy.png',
-// }, function(err, url) {
-//   console.log(err);
-//   console.log(url);
-// });
+// local
+if (process.env.PORT) {
+  AWS.config.update({
+    // region: 'Tokyo',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+}
+else {
+  AWS.config.loadFromPath(path.join(__dirname, 'credentials.json'));
+}
+var s3 = new AWS.S3();
 
 
 app.set('port', process.env.PORT || 3001);
@@ -75,29 +63,78 @@ app.get('/shot', function(req, res) {
     renderDelay: 4,
   };
 
-  // あればそれを返す
-  if (fs.existsSync(output)) {
-    res.sendFile(path.join(__dirname, '../', output));
-  }
-  // キュー待ちも除外
-  else {
-    res.sendFile(path.join(__dirname, '../static/images/dummy.png'));
-    shot.shot(url, output, options, function(err) {
-      console.log(err);
-      // res.sendFile(path.join(__dirname, '../', output));
-    });
-  }
+  s3.getObject({
+    Bucket: 'runstant-shot',
+    Key: name,
+  }, function(err, data) {
+    // データがあればそれを返す
+    if (data) {
+      console.log('hoge');
+      res.writeHead(200, {'Content-Type': data.ContentType });
+      res.end(data.Body);
+    }
+    // なければダミーを返してショットキューに入れる
+    else {
+      res.sendFile(path.join(__dirname, '../static/images/dummy.png'));
+      shot.shot(url, output, options, function(err) {
+        // console.log(err);
+
+        var img = fs.readFileSync(path.join(__dirname, '../', output));
+        s3.putObject({
+          Bucket: 'runstant-shot',
+          Key: name,
+          ContentType: 'image/png',
+          ACL: 'public-read',
+          Body: img,
+        }, function(err, data) {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log('success upload ' + name + ' to s3.');
+          }
+        });
+      });
+    }
+  });
 });
 
+// queue の状態を見れるようにする
+app.get('/queue', function(req, res) {
+
+});
+
+
 app.get('/hoge', function(req, res) {
+
+  // var img = fs.readFileSync('static/images/13ab8409.png');
+  // res.writeHead(200, {'Content-Type': 'image/png' });
+  // res.end(img);
+
+  // return ;
+
 
   s3.getObject({
     Bucket: 'runstant-shot',
     Key: 'dummy.png',
-  }, function(err, url) {
-    console.log('***************');
-    console.log(url);
+  }, function(err, data) {
+    console.log(data);
+    res.writeHead(200, {'Content-Type': data.ContentType });
+    res.end(data.Body);
   });
+
+
+  // var img = fs.readFileSync('static/images/13ab8409.png');
+  // s3.putObject({
+  //   Bucket: 'runstant-shot',
+  //   Key: 'aaaaa.txt',
+  //   Body: 'Hello, BBBB!',
+  //   // Key: 'hoge.png',
+  //   // Body: img,
+  // }, function(err, data) {
+  //   console.log(data);
+  //   res.send('success update!');
+  // });
 
 });
 
