53a3377249b3be5ef166e95ad8f4f7945cd20500
phi <phi1618jp@gmail.com>
2016-03-03 20:16:28 +0900

    use capstant

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ var webshot = require('webshot');
 var Nightmare = require('nightmare');
 var nightmare = null;
 var shot = require('./lib/shot.js');
+var capstant = require('capstant');
 var app = express();
 
 var phantomPath = (function() {
@@ -50,8 +51,70 @@ app.use(require('less-middleware')(path.join(__dirname, 'static')));
 app.use(express.static(path.join(__dirname, '../static')));
 
 app.get('/', function (req, res) {
-  res.send('index');
-  // res.render('index');
+  res.render('index');
+});
+
+
+var queue = [];
+app.get('/shot/:size', function(req, res) {
+  var key = crc.crc32(req.originalUrl).toString(16);
+  var url = req.query.url;
+  var output = 'static/images/' + key + '.png';
+  var delay = req.query.delay || 0;
+  var sizes = req.params.size.split('x');
+  var width = +sizes[0];
+  var height = +sizes[1];
+  var options = {
+    phantomPath: phantomPath,
+    url: url,
+    output: output,
+    width: width,
+    height: height,
+    delay: delay,
+  };
+
+  s3.getObject({
+    Bucket: 'runstant-shot',
+    Key: key,
+  }, function(err, data) {
+    if (data) {
+      res.writeHead(200, {'Content-Type': data.ContentType });
+      res.end(data.Body);
+    }
+    else {
+      res.sendFile(path.join(__dirname, '../static/images/dummy.png'));
+
+      if (queue.indexOf(key) !== -1) {
+        console.log('キューにあるので無視');
+        return ;
+      }
+      
+      // キューに追加
+      queue.push(key);
+
+      // スクショ
+      capstant.shot(url, output, options).then(function() {
+        var img = fs.readFileSync(path.join(__dirname, '../', output));
+        s3.putObject({
+          Bucket: 'runstant-shot',
+          Key: key,
+          ContentType: 'image/png',
+          ACL: 'public-read',
+          Body: img,
+        }, function(err, data) {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log('success upload ' + key + ' to s3.');
+            // キューから削除
+            queue.splice(queue.indexOf(key), 1);
+          }
+        });
+
+      });
+    }
+  });
 });
 
 app.get('/shot', function(req, res) {
